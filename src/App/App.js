899e5d629952d90964ff26963fb45e330f0f177c
Palych18 <pavel_egoroff@mail.ru>
2024-03-13 15:41:45 +0300

    Edited App

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,20 @@
 import './styles/index.scss';
+import { useEffect } from 'react';
 import { Slider } from 'features';
+import { usePhotos } from 'shared';
 
 export const App = () => {
+  const photosState = usePhotos();
+
+  useEffect(() => {
+    photosState.getPhotos(4);
+  }, []);
+
+  console.log(photosState.photos);
+
   return (
     <div className={'app'}>
-      {/* Slider */}
-      <Slider slides={[]}/>
+      <Slider slides={[]} />
     </div>
   );
 };
